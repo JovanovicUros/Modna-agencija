c009db561022570bd17aafc1530b4809d0aafae3
JovanovicUros <148771936+JovanovicUros@users.noreply.github.com>
2026-04-25 01:06:44 +0200

    Add files via upload

---
+++ Welcome.pas
@@ -0,0 +1,40 @@
+﻿unit Welcome;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.StdCtrls, FMX.Controls.Presentation;
+
+type
+  TModnaAgencija = class(TForm)
+    Rectangle1: TRectangle;
+    Label1: TLabel;
+    Label2: TLabel;
+    Rectangle2: TRectangle;
+    Z: TLabel;
+    Image1: TImage;
+    Image3: TImage;
+    Image2: TImage;
+    procedure Rectangle1Click(Sender: TObject);
+  private
+  public
+  end;
+
+var
+  ModnaAgencija: TModnaAgencija;
+
+implementation
+
+uses Login;   // povezivanje sa login formom
+
+{$R *.fmx}
+
+procedure TModnaAgencija.Rectangle1Click(Sender: TObject);
+begin
+  Log_in.Show;        // otvara login stranicu
+  ModnaAgencija.Hide; // sakriva welcome
+end;
+
+end.
